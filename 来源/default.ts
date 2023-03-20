502fae5f064fee282dba245e1593526e3180550f
mxnf1314 <128242420+mxnf1314@users.noreply.github.com>
2023-03-20 22:47:14 +0800

    Update default.ts

--- 来源/default.ts
+++ 来源/default.ts
@@ -7,6 +7,7 @@ export const defaultSetting = {
 }
 
 export const defaultMessage = `
-- 本站仅用于演示，填入自己的 key 才可使用555。
-- 由 [是枭](https://blog.evv1.com) 站点源码出售，功能强大响应快，欢迎自部署。
-- **Shift+Enter** 换行。开头输入 **/** 或者 **空格** 搜索 Prompt 预设。点击输入框滚动到底部。`
+- 本站仅用于演示，填入自己的 key 才可使用。
+- 联系站长 [每日免费分享key](http://mxnf.store/234.png) 也可个位数获取私人专享账号key
+-  [官网关注不迷路](http://mxnf.tk) 网站出问题，请联系站长
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
