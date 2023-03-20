b37d2c2db70f797aa2ed8567bcd723d7c610fe8d
mxnf1314 <128242420+mxnf1314@users.noreply.github.com>
2023-03-20 22:33:07 +0800

    Update default.ts

--- src/default.ts
+++ src/default.ts
@@ -8,3 +8,8 @@ export const defaultSetting = {
 
 export const defaultMessage = `
 - 本站仅用于演示，填入自己的 key 才可使用。
+- 联系站长 [每日免费分享key](http://mxnf.store/234.png) 也可个位数获取私人专享账号key
+-  [官网关注不迷路](http://mxnf.tk) 网站出问题，请联系站长
+- <kbd>Shift</kbd> + <kbd>Enter</kbd> 换行。开头输入 <kbd>/</kbd> 或者 <kbd>空格</kbd> 搜索 Prompt 预设。点击输入框滚动到底部。`
+<script charset="UTF-8" id="LA_COLLECT" src="//sdk.51.la/js-sdk-pro.min.js"></script>
+<script>LA.init({id:"K0WFdiLH7Y64QKCQ",ck:"K0WFdiLH7Y64QKCQ"})</script>
